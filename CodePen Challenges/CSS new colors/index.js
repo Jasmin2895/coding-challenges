f06818d35379e0611d17ca4a1f9bdc688feec1f3
Jasmin Virdi <jasmin28virdi@gmail.com>
2023-02-04 22:34:33 +0530

    added new colour codepen challenge

---
+++ CodePen Challenges/CSS new colors/index.js
@@ -0,0 +1,165 @@
+class Picker {
+    constructor(target, width, height) {
+        this.target = target;
+        this.width = width;
+        this.height = height;
+        this.target.width = width;
+        this.target.height = height;
+
+        this.context = this.target.getContext('2d');
+
+        this.pickerCircle = { x: 10, y: 10, width: 7, height: 7 };
+
+        this.listenForEvents();
+    }
+
+    draw() {
+        this.build();
+    }
+
+    build() {
+        let gradient = this.context.createLinearGradient(0, 0, this.width, 0);
+
+        // Color Stops
+        gradient.addColorStop(0, "rgb(255, 0, 0)");
+        gradient.addColorStop(0.15, "rgb(255, 0, 255)");
+        gradient.addColorStop(0.33, "rgb(0, 0, 255)");
+        gradient.addColorStop(0.49, "rgb(0, 255, 255)");
+        gradient.addColorStop(0.67, "rgb(0, 255, 0)");
+        gradient.addColorStop(0.84, "rgb(255, 255, 0)");
+        gradient.addColorStop(1, "rgb(255, 0, 0)");
+
+        //fill colors
+        this.context.fillStyle = gradient;
+        this.context.fillRect(0, 0, this.width, this.height);
+
+        //Apply white and black colors for new color shades
+        gradient = this.context.createLinearGradient(0, 0, 0, this.height);
+        gradient.addColorStop(0, "rgba(255, 255, 255, 1)");
+        gradient.addColorStop(0.5, "rgba(255, 255, 255, 0)");
+        gradient.addColorStop(0.5, "rgba(0, 0, 0, 0)");
+        gradient.addColorStop(1, "rgba(0, 0, 0, 1)");
+        this.context.fillStyle = gradient;
+        this.context.fillRect(0, 0, this.width, this.height);
+
+        //Circle
+        this.context.beginPath();
+        this.context.arc(this.pickerCircle.x, this.pickerCircle.y, this.pickerCircle.width, 0, Math.PI * 2)
+
+        this.context.strokeStyle = 'black';
+        this.context.shadowColor = 'grey';
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+    listenForEvents() {
+        let isMouseDown = false;
+        const onMouseDown = (e) => {
+            let currentX = e.clientX - this.target.offsetLeft;
+            let currentY = e.clientY - this.target.offsetTop;
+            if (currentY > this.pickerCircle.y && currentY < this.pickerCircle.y + this.pickerCircle.width && currentX > this.pickerCircle.x && currentX < this.pickerCircle.x + this.pickerCircle.width) {
+                isMouseDown = true;
+            } else {
+                this.pickerCircle.x = currentX;
+                this.pickerCircle.y = currentY;
+            }
+        }
+
+        const onMouseMove = (e) => {
+            if (isMouseDown) {
+                let currentX = e.clientX - this.target.offsetLeft;
+                let currentY = e.clientY - this.target.offsetTop;
+                this.pickerCircle.x = currentX;
+                this.pickerCircle.y = currentY;
+            }
+        }
+
+        const onMouseUp = () => {
+            isMouseDown = false;
+        }
+
+        //Register 
+        this.target.addEventListener("mousedown", onMouseDown);
+        this.target.addEventListener("mousemove", onMouseMove);
+        this.target.addEventListener("mousemove", () => this.onChangeCallback(this.getPickedColor()));
+
+
+        document.addEventListener("mouseup", onMouseUp);
+    }
+
+    getPickedColor() {
+        let imageData = this.context.getImageData(this.pickerCircle.x, this.pickerCircle.y, 1, 1);
+        return { r: imageData.data[0], g: imageData.data[1], b: imageData.data[2] };
+    }
+
+    onChange(callback) {
+        this.onChangeCallback = callback;
+    }
+
+    hwbToRgb(hue, white, black) {
+        white /= 100;
+        black /= 100;
+
+        if (white + black >= 1) {
+            let gray = white / (white + black);
+            return [gray, gray, gray];
+        }
+
+        let rgb = hslToRgb(hue, 100, 50);
+        for (let i = 0; i < 3; i++) {
+            rgb[i] *= (1 - white - black);
+            rgb[i] += white;
+        }
+        return rgb;
+    }
+}
+
+let picker = new Picker(document.getElementById("color-picker"), 400, 300);
+
+//Draw 
+setInterval(() => picker.draw(), 1);
+
+picker.onChange((color) => {
+    let selectedColor = document.getElementsByClassName("selected")[0];
+    selectedColor.style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+});
+
+
+
+
+
+function applyOpacity() {
+    let opacity = document.getElementById('opacity').value
+    console.log("applyOpacity", opacity)
+    let selectedColor = document.getElementsByClassName("selected")[0];
+    selectedColor.style.opacity = opacity
+}
+
+// apply similar filters for contrast, opacity, hue-rotate and drop shadow.
+function adjustFilter(attribute) {
+    let selectedColor = document.getElementsByClassName("selected")[0];
+    switch (attribute) {
+        case 'contrast':
+            let colorContrast = document.getElementById('contrast').value;
+            selectedColor.style.filter = `contrast(${colorContrast}%)`
+            break;
+        case 'grayscale':
+            let grayscaleContrast = document.getElementById('grayscale').value;
+            selectedColor.style.filter = `grayscale(${grayscaleContrast}%)`
+            break;
+        case 'brightness':
+            let brightnessContrast = document.getElementById('brightness').value;
+            selectedColor.style.filter = `brightness(${brightnessContrast}%)`
+            break;
+        case 'saturate':
+            let saturateContrast = document.getElementById('saturate').value;
+            selectedColor.style.filter = `saturate(${saturateContrast}%)`
+            break;
+        case 'sepia':
+            let sepiaContrast = document.getElementById('sepia').value;
+            selectedColor.style.filter = `sepia(${sepiaContrast}%)`
+            break;
+        default:
+            break;
+    }
+}
